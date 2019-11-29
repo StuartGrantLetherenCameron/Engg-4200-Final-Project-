b09b71ff60d610edccc401b3ae344dd55c6e1552
Kevin <kevincbarnfield@gmail.com>
2019-11-29 17:28:51 -0500

    Update Coordinator_Game.ino

--- Coordinator_Game/Coordinator_Game.ino
+++ Coordinator_Game/Coordinator_Game.ino
@@ -1,8 +1,10 @@
 /*
- * ENGG*4200 Project
- * Whack a Mole
- * Coordinator/Base/Game Node
- */
+* ENGG*4200 Project
+* Whack a Mole
+* Coordinator/Base/Game Node
+*/
+
+#include <LiquidCrystal.h>
 
 // Circuit parameters
 const int numNodes = 3;
@@ -11,13 +13,15 @@ const char rcv[numNodes] = {'b', 'd', 'e'};
 const int RED = 7;
 const int GRN = 8;
 
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+
 // Game parameters
 bool active = true;
 int dest = 0;
 int prev = 0;
 int score = 0;
 
-int duration = 20;
+int duration = 30;
 int timer = duration * 1000;
 
 // Communications
@@ -27,6 +31,9 @@ void setup() {
   // Hardware setup
   pinMode(RED, OUTPUT);
   pinMode(GRN, OUTPUT);
+
+  lcd.begin(16, 2);
+  lcd.print("Whack a mole!");
   
   // Establish communication to XBee
   Serial.begin(9600);
@@ -37,20 +44,20 @@ void setup() {
 }
 
 void loop() {
+  
   // Continue if game still active, else break.
   if (active == true) {
-    digitalWrite(RED, HIGH);
-    digitalWrite(GRN, LOW);
-  } else {
     digitalWrite(RED, LOW);
     digitalWrite(GRN, HIGH);
+  } else {
+    digitalWrite(RED, HIGH);
+    digitalWrite(GRN, LOW);
     Serial.print('X');
     return;
-    } 
+  }
 
   // Tell current destination to output
   Serial.print(node[dest]);
-  //Serial.print('D');
 
   // Check for new data on Serial
   if (Serial.available() > 0) {
@@ -59,8 +66,16 @@ void loop() {
     // User pressed dest button
     if (incoming == rcv[dest]) {
       score = score + 1;
+      lcd.setCursor(0, 1);
+      lcd.print("Score: ");
+      lcd.print(score);
+      
       prev = dest;
       while (dest == prev) {
+        duration = duration - 1;
+        if (duration < 10) {
+          duration = 10;
+        }
         timer = duration * 1000;
         dest = random(numNodes);
       }
